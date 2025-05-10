bee5505c948babe96a8e23feeb78f570241e0a67
Bryce Bostwick <github@brycebostwick.com>
2025-05-09 21:14:17 -0700

    Initial Commit

---
+++ WidgetAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WidgetAnimation
+//
+//  Created by Bryce Pauken on 5/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WidgetAnimation/WidgetAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetAnimationApp.swift
+//  WidgetAnimation
+//
+//  Created by Bryce Pauken on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct WidgetAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
